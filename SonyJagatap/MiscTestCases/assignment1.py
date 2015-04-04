d2d41e8d8eeec7ac74ba4e577c9e627724bbd08c
sonyjagatap <sonyjagatap@yahoo.com>
2015-04-04 15:23:54 -0700

    assignment1.py - first python assignment

---
+++ SonyJagatap/MiscTestCases/assignment1.py
@@ -0,0 +1,19 @@
+print "arthamatic op on 2 numbers"
+a=60
+b=30
+c=a+b
+print "a=",a
+print "b=",b
+print"sum=", c
+
+c=a-b
+print "difference=", c
+
+c=a*b
+print "product=", c
+
+c=a/b
+print "quotient=", c 
+
+c=a%b
+print "remainder=", c
