7065d42692103830d9c37c6937801d88586e7733
sonyjagatap <sonyjagatap@yahoo.com>
2015-04-04 15:34:08 -0700

    assignment2

---
+++ SonyJagatap/MiscTestCases/assignment2.py
@@ -0,0 +1,26 @@
+print "arthamatic op on 3 numbers"
+a=400
+b=200
+c=80
+d=a+b+c
+print "a=",a
+print "b=",b
+print "c=",c
+print "sum=", d
+
+d=a-b-c
+print "difference=", d
+
+d=a*b*c
+print "product=", d
+
+d=a/b/c
+print "quotient=",d
+
+d=a%b%c
+print "remainder=", d  
+  
+d=a+b-c
+print d 
+ 
+
